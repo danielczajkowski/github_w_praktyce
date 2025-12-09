e67f5719aa1fe576dfee43270f376bcecc0b6f79
Daniel Czajkowski <danielczajkowski0@gmail.com>
2025-12-09 07:49:58 +0100

    CHORE: Configure Storybook and clean up CRA boilerplate
    - Initialize Storybook with default configuration
    - Remove unused Create React App files (logo, tests)
    - Update project dependencies

---
+++ .storybook/preview.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import GlobalStyle from '../src/theme/GlobalStyle';
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <>
+        <GlobalStyle />
+        <Story />
+      </>
+    ),
+  ],
+};
+
+export default preview;
