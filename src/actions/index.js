41bd8b25cd7d13ae0ed9a1d4cafd72aed3446e81
Daniel Czajkowski <danielczajkowski0@gmail.com>
2025-12-17 14:22:23 +0100

    FEAT: Implement async Redux actions for API integration - Add authenticateUser thunk for login/register - Add logout thunk for session termination - Add fetchItems thunk with correct payload structure (data + itemType) - Add fetchSingleItem thunk for details view - Define action types constants

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,20 @@
+import axios from 'axios';
+
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const ADD_ITEM = 'ADD_ITEM';
+export const AUTH_REQUEST = 'AUTH_REQUEST';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAILURE = 'AUTH_FAILURE';
+export const FETCH_REQUEST = 'FETCH_REQUEST';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAILURE = 'FETCH_FAILURE';
+export const FETCH_SINGLE_ITEM = 'FETCH_SINGLE_ITEM';
+export const FETCH_SINGLE_ITEM_SUCCESS = 'FETCH_SINGLE_ITEM_SUCCESS';
+export const FETCH_SINGLE_ITEM_FAILURE = 'FETCH_SINGLE_ITEM_FAILURE';
+export const LOGOUT = 'LOGOUT';
+
 export const removeItem = (itemType, id) => ({
-  type: 'REMOVE_ITEM',
+  type: REMOVE_ITEM,
   payload: { itemType, id },
 });
 
@@ -9,7 +24,7 @@ export const addItem = (itemType, item) => {
   };
 
   return {
-    type: 'ADD_ITEM',
+    type: ADD_ITEM,
     payload: {
       itemType,
       item: {
@@ -18,4 +33,94 @@ export const addItem = (itemType, item) => {
       },
     },
   };
+};
+
+export const authenticateUser =
+  (username, password, endpoint = 'login') =>
+  (dispatch) => {
+    dispatch({
+      type: AUTH_REQUEST,
+    });
+
+    return axios
+      .post(`http://localhost:9000/api/user/${endpoint}`, {
+        username,
+        password,
+      })
+      .then((payload) => {
+        dispatch({
+          type: AUTH_SUCCESS,
+          payload,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch({
+          type: AUTH_FAILURE,
+        });
+      });
+  };
+
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT });
+  return axios
+    .post('http://localhost:9000/api/user/logout', {}, { withCredentials: true })
+    .then(() => {
+      console.log('Logout successful');
+    })
+    .catch((error) => {
+      console.error('Logout failed:', error);
+    });
+};
+
+export const fetchItems = (itemType) => (dispatch, getState) => {
+  dispatch({
+    type: FETCH_REQUEST,
+  });
+
+  return axios
+    .get('http://localhost:9000/api/notes/type', {
+      params: {
+        type: itemType,
+        userID: getState().user.id,
+      },
+    })
+    .then(({ data }) => {
+      dispatch({
+        type: FETCH_SUCCESS,
+        payload: {
+          data,
+          itemType,
+        },
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      dispatch({
+        type: FETCH_FAILURE,
+      });
+    });
+};
+
+export const fetchSingleItem = (itemType, id) => (dispatch) => {
+  dispatch({
+    type: FETCH_SINGLE_ITEM,
+  });
+
+  return axios
+    .get(`http://localhost:9000/api/note/${id}`)
+    .then(({ data }) => {
+      dispatch({
+        type: FETCH_SINGLE_ITEM_SUCCESS,
+        payload: {
+          data,
+        },
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      dispatch({
+        type: FETCH_SINGLE_ITEM_FAILURE,
+      });
+    });
 };
